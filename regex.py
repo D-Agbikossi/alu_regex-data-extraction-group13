1ea1269792bfb95f385a1c9e39ca9a0eb4dca6fe
BayinganaEdwin <e.bayingana@alustudent.com>
2024-09-15 12:04:10 +0200

    add the HTML Tag regular expression

--- regex.py
+++ regex.py
@@ -24,7 +24,7 @@ pattern5 = r'\b(?:[01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?\b'
 timeStamps = re.findall(pattern5, file_content)
 print("Time Stamps found:", timeStamps)
 
-# Pattern for HTML tags and find matches
+# Pattern for HTML tags matches
 pattern6 = r'<\/?[a-zA-Z][a-zA-Z0-9\s"\'=.-]*>'
 html_tags = re.findall(pattern6, file_content)
 print("HTML Tags found:", html_tags)
